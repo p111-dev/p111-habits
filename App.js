20a86754a4051daab08e719fa6cc4c649dc0e7c6
Mario Marin <mariomarinteran@gmail.com>
2025-02-20 21:50:35 +0100

    merge

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< Updated upstream
       <Text>Hola esta es la rama de mario</Text>
+=======
+      <Text>Texto de mario</Text>
+      <Text>Texto añadido por Robert...</Text>
+      <Button> Hola soy un boton </Button>
+>>>>>>> Stashed changes
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
